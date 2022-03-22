55afb96fb3e687f313e08d070871c52a8add3c4c
Stan <stantonmoore42@gmail.com>
2022-03-22 14:31:23 -0400

    Working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import Receipt from "./Receipt";
 
-function App() {
+export default function App() {
+  const receipt1 =
+  {
+    person: 'Karolin',
+    order: {
+      main: 'Burrito',
+      protein: 'Organic Tofu',
+      rice: 'Purple Rice',
+      sauce: 'Green Crack',
+      toppings: [
+        'Baby Bok Choy, ', 'Cucumber Kimchi '
+      ],
+      drink: 'Korchata',
+      cost: 22
+    },
+    paid: false
+  }
+  const receipt2 = {
+    person: 'Jerrica',
+    order: {
+      main: 'Rice Bowl',
+      protein: 'Ginger Soy Chix',
+      rice: 'Sticky Rice',
+      sauce: 'Korilla',
+      toppings: [
+        'Yuzu Pickled Sweet Pepper, ', 'Kale '
+      ],
+      drink: 'Korchata',
+      cost: 19
+    },
+    paid: false
+  }
+  const receipt3 = {
+    person: 'Matt',
+    order: {
+      main: 'Salad Bowl',
+      protein: 'Organic Tofu',
+      rice: 'none',
+      sauce: "K'lla",
+      toppings: [
+        'Blue Potato Salad, ', 'Pico De Gallo, ', 'Red Kimchi '
+      ],
+      drink: 'Sparkling Blood Orange Soda',
+      cost: 20
+    },
+    paid: true
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Receipt   
+        person={receipt1.person}
+        order={receipt1.order}
+        paid={receipt1.paid}
+      />
+      <Receipt   
+        person={receipt2.person}
+        order={receipt2.order}
+        paid={receipt2.paid}
+      />
+      <Receipt   
+        person={receipt3.person}
+        order={receipt3.order}
+        paid={receipt3.paid}
+      />
     </div>
   );
 }
-
-export default App;
---
+++ src/Receipt.jsx
@@ -0,0 +1,19 @@
+export default function Receipt(props) {
+    console.log(props)
+    const { person , order, paid } = props
+    return (
+        <div className="person-card">
+            <h1>{person}</h1>
+            <div className="order-card">
+                <p>Main: {order.main}</p>
+                <p>Protein: {order.protein}</p>
+                <p>Rice: {order.rice}</p>
+                <p>Sauce: {order.sauce}</p>
+                <p>Toppings: {order.toppings}</p>
+                <p>Drink: {order.drink}</p>
+                <p>Cost: {order.cost}</p>
+                <p>Paid: {paid}</p>
+            </div>
+        </div>
+    )
+}    
